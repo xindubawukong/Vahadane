af87cde74a5ac1cb56145b4a7d2a1be0ae6224e7
xindubawukong <xindubawukong@outlook.com>
2019-01-29 07:39:26 +0800

    done, but not right

--- global_config.py
+++
@@ -1,6 +0,0 @@
-SOURCE_PATH = './data/i2.png'
-TARGET_PATH = './data/i1.png'
-RESULT_PATH = './output/i2_to_i1.png'
-
-STAIN_NUM = 2
-LAMBDA = 0.02
--- vahadane.py
+++ vahadane.py
@@ -1,32 +1,52 @@
 import spams
 import numpy as np
 import cv2
-from global_config import *
+
+
+STAIN_NUM = 2
+LAMBDA = 0.02
 
 
 def getV(img):
-    img_LAB = cv2.cvtColor(img, cv2.COLOR_RGB2LAB)
-    mask = img_LAB[:, :, 0] / 255 < 0.9
-    I = img[mask].reshape((-1, 3))
-    mask = (I == 0)
-    I[mask] = 1
-    V = np.log(255 / I)
-    return V
+    I0 = img.reshape((-1,3)).T
+    mask = (I0 == 0)
+    I0[mask] = 1
+    V0 = np.log(255 / I0)
+    
+#     img_LAB = cv2.cvtColor(img, cv2.COLOR_RGB2LAB)
+#     mask = img_LAB[:, :, 0] / 255 < 0.9
+#     I = img[mask].reshape((-1, 3)).T
+#     mask = (I == 0)
+#     I[mask] = 1
+#     V = np.log(255 / I)
+    return V0
 
 
 def getW(V):
-    W = spams.trainDL(V.T, K=STAIN_NUM, lambda1=LAMBDA, mode=2, modeD=0, posAlpha=True, posD=True).T
-    W = W / np.linalg.norm(W, axis=1)[:, None]
+    W = spams.trainDL(V, K=STAIN_NUM, lambda1=LAMBDA, mode=2, modeD=0, posAlpha=True, posD=True)
+    W = W / np.linalg.norm(W, axis=0)[None, :]
     return W
 
 
 def getH(V, W):
-    return 0
+    H = spams.lasso(V, W, mode=2, lambda1=LAMBDA, pos=True)
+    return H.toarray()
 
 
-def stain_separate(img, flag=True):
+def SNMF(img, flag=True):
     V = getV(img)
     W = getW(V)
-    print('W:\n', W)
     H = getH(V, W)
     return W,H
+
+
+def SPCN(img, Ws, Hs, Wt, Ht):
+    Hs_RM = np.percentile(Hs, 99, axis=1)
+    Ht_RM = np.percentile(Ht, 99, axis=1)
+    Hs_norm = Hs * Ht_RM[:, None] / Hs_RM[:, None]
+    print(Ws)
+    print(Wt)
+    Vs_norm = np.dot(Wt, Hs)
+    Is_norm = 255 * np.exp(-1 * Vs_norm)
+    I = Is_norm.T.reshape(img.shape).astype(np.uint8)
+    return I
